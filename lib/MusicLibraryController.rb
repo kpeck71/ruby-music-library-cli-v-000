db6531e31df4fc1f3e0efa5150e61077b922388f
Kristen Peck <kpeck71@gmail.com>
2017-10-23 00:41:27 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -38,7 +38,7 @@ class MusicLibraryController
   end
 
   def list_songs #Song all should have unique names only?
-   Song.all.uniq.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |song, index|
+   Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |song, index|
      puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
 
